ce22ded9442dd4f95cd565f0a000b961bcf83f8e
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2021-12-09 02:36:38 +0700

    background video added

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,6 +1,16 @@
 <template>
   <header class="header">
     <img alt="logo" src="~/assets/img/logo.png" class="header__logo">
+    <nav class="header__panel">
+      <nuxt-link
+        v-for="tab in tabs"
+        :key="tab.id"
+        :to="tab.link"
+        class="header__tab"
+      >
+        {{ tab.label }}
+      </nuxt-link>
+    </nav>
   </header>
 </template>
 
@@ -9,6 +19,28 @@ import MainVue from '../../mixins/MainVue';
 
 export default MainVue.extend({
   name: 'header-element',
+  data() {
+    return {
+      tabs: [
+        {
+          id: 0,
+          link: '/',
+          label: 'Курсы',
+        },
+        {
+          id: 1,
+          link: '/44',
+          label: 'О нас',
+        },
+        {
+          id: 2,
+          link: '/123',
+          label: 'Личный кабинет',
+        },
+
+      ],
+    };
+  },
 });
 </script>
 
@@ -23,11 +55,43 @@ export default MainVue.extend({
   left: 0;
   z-index: 1000;
   height: 80px;
-  padding: 20px;
+  padding: 20px 40px 20px 20px;
   &__logo {
     margin-top: 7px;
     width: 264px;
     height: 54px;
   }
+  &__panel {
+    height: 40px;
+    display: flex;
+    align-items: center;
+  }
+  &__tab {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 18px;
+    background-color: transparent;
+    color: $white;
+    font-family: $header-font;
+    font-size: 14px;
+    line-height: 16px;
+    border-radius: 50px;
+    cursor: pointer;
+    transition: $transition-header;
+    margin-right: 20px;
+    &:last-child {
+      margin-right: 0;
+    }
+    &:hover {
+      background-color: $yellow-main;
+      color: $black;
+    }
+    &:active {
+      background-color: $yellow-active;
+      color: $black;
+    }
+  }
+
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="landing">
-    <div class="landing__banner">
-      1
-    </div>
-    asdfafsd
+    <video class="landing__banner banner" autoplay muted loop>
+      <source src="https://www.kernelios.com//wp-content//uploads//2020//12//VideoB.mp4" type="video/mp4">
+      <div class="banner__top">
+        <img class="banner__logo" alt="logo" src="~/assets/img/circle-logo.png">
+        <h1 class="banner__title">
+          Simulating
+          <br>
+          Cyber Threats
+        </h1>
+      </div>
+      <div class="banner__text">
+        TRAINING TODAY’S LEADERS FOR TOMORROW’S THREATS
+      </div>
+    </video>
   </div>
 </template>
 
@@ -20,13 +30,38 @@ export default MainVue.extend({
 <style lang="scss" scoped>
 .landing {
   @include container;
-  background-color: cadetblue;
   height: 900px;
   &__banner {
-    width: 100%;
+    width: 100vw;
     height: 100vh;
-    background-image: url(~/assets/img/header.gif);
-    background-size: 100% 100%;
+    object-fit: fill;
+  }
+  .banner {
+    //padding-left: 48%;
+    //padding-top: 420px;
+    &__top {
+      display: flex;
+      align-items: center;
+
+      margin-bottom: 10px;
+    }
+    &__logo {
+      width: 103px;
+      height: 103px;
+      margin-right: 10px;
+    }
+    &__title {
+      color: $white;
+      font-size: 60px;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+    &__text {
+      display: flex;
+      color: $yellow-main;
+      font-size: 19px;
+      font-weight: 500;
+    }
   }
 }
 </style>
